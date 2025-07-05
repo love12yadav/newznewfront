c0ce775d339c7c6f68319ea62e241210a5694b68
Love yadav <155807335+love12yadav@users.noreply.github.com>
2025-07-05 16:30:34 +0530

    Update LoginPage.jsx

--- src/Component/LoginPage.jsx
+++ src/Component/LoginPage.jsx
@@ -14,7 +14,7 @@ function LoginPage() {
     const loginData = { email, password };
 
     try {
-      const response = await fetch('http://localhost:8080/auth/signing', {
+      const response = await fetch('https://newzback-1.onrender.com/auth/signing', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
